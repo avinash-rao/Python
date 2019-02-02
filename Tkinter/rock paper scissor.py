76f0ad6810a7e32098a11518bc9edda07890c4c7
Avinash Rao <avinash_it@yahoo.com>
2019-02-02 11:48:40 +0530

    Added frames

--- Tkinter/rock paper scissor.py
+++ Tkinter/rock paper scissor.py
@@ -5,7 +5,7 @@ global rps
 rps = ["rock", "paper", "scissor"]
 global compChoice
 global userChoice
-userChoice = 0
+userChoice = None
 compChoice = None
 
 def Rock(label):
@@ -34,7 +34,7 @@ def display(label):
     global userChoice
     global compChoice
     global rps
-    str = "You: " + rps[userChoice-1] + "\nComputer: " + rps[compChoice-1] + '\n\n'
+    str = "You: " + rps[userChoice-1] + "\n\nComputer: " + rps[compChoice-1] + '\n\n'
     label['text'] = str
     if userChoice == compChoice:
         label['text'] = '\n\n'.join([str, "Tie"])
@@ -46,15 +46,22 @@ def display(label):
 
 root = Tk()
 root.title("Rock Paper Scissor")
-# root.minsize(width=750, height=250)
-rock = Button(root, text="Rock", command=lambda:Rock(label))
-paper = Button(root, text="Paper", command=lambda:Paper(label))
-scissor = Button(root, text="Scissor", command=lambda:Scissor(label))
+root.minsize(width=300, height=250)
+
+# write frame1 and frame2 here
+frame1 = Frame(root, padx=5, pady=5)
+frame2 = Frame(root, padx=5, pady=5)
+frame1.pack(side=TOP)
+frame2.pack(side=BOTTOM)
+
+rock = Button(frame1, text="Rock", command=lambda:Rock(label), padx=30, pady=20)
+paper = Button(frame1, text="Paper", command=lambda:Paper(label), padx=30, pady=20)
+scissor = Button(frame1, text="Scissor", command=lambda:Scissor(label), padx=30, pady=20)
 rock.pack(side=LEFT)
 paper.pack(side=LEFT)
 scissor.pack(side=LEFT)
 
-label = Label(root)
+label = Label(frame2)
 label.pack(side=BOTTOM)
 
 root.mainloop()
